811f8d9c08258579eea2b7440cb3f1ca5ebef97c
Naderadelp <naderadelpp@gmail.com>
2025-08-12 00:04:42 +0300

    some changes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisterController::class,'register']);
 Route::post('/login', [RegisterController::class,'login']);
+Route::apiResource('/tickets', TicketController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
